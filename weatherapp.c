7df42826395536f2cffe930318e9ba98f273b3c9
YourAverageProgrammer924 <56584652+deadprocess@users.noreply.github.com>
2025-04-19 18:32:36 +0200

    I have errors in  line 28 and 35 need to check on that later

--- weatherapp.c
+++ weatherapp.c
@@ -21,8 +21,19 @@ int main () { // Hauptfunktion
     CURLcode res = api_call(); // Aufruf meiner eigenen Funktion api_call, welche die API abfragt und das Resultat als Typ CURLcode zurückgibt und es in der Variable res abspeichert
     if (res == CURLE_OK) { // Überprüfung ob mein API-Call erforgreich war
     printf("API call war erfolgreich\n"); //Ausgabe einer Erfolgsnachricht
-    } else {
+    } else { 
         printf("Der API call war nicht erfolgreich.\n"); // Ausgabe einer Fehlermeldung
     }
 
+    const char *json_string = res; //Hier wird der Rückgabewert der API in einen String umgewandelt, damit er als JSON interpretiert werden kann
+
+    //Parse JSON
+    struct json_object *parsed_json;
+    parsed_json = json_tokener_parse(json_string); //Hier wird der JSON-String geparsed und in parsed_json gespeichert
+    struct json_object *weather_description;
+    json_object_object_get_ex(parsed_json, "weather_description", &weather_description); //Hier wird das Wetter-Objekt aus dem JSON-String extrahiert und in weather_description gespeichert
+    struct json_object *weather_description = json_object_array_get_idx(weather_description, 0); //Hier wird das erste Element des Wetter-Objekts extrahiert und in weather_description gespeichert
+    struct json_object *value;
+    json_object_object_get_ex(value, "value", &value); //Hier wird der Wert des Wetter-Objekts extrahiert und in value gespeichert
+    printf("Wetterbeschreibung: %s\n", json_object_get_string(value)); //Hier wird die Wetterbeschreibung ausgegeben
 }
